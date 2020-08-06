2d3775ea3957fa77159a7581d2f37a1861f3bd9f
Anwesh <anweshm136@gmail.com>
2020-08-07 01:52:12 +0530

    Final version. Noise free versions in output folder

--- trial.py
+++ trial.py
@@ -1,18 +1,15 @@
 import subprocess
 import os
+from pydub import AudioSegment
 
-ip_path="input_raw/"
-op_path="output_raw/"
-
-for filename in os.listdir(ip_path):
-    filename=os.path.splitext(filename)[0]
-    #print(filename)
-    subprocess.call(["./examples/rnnoise_demo", ip_path+filename+".raw", op_path+filename+"_proc.raw"])
-
-ip_path="output_raw/"
+ip_path="input/"
+ipraw_path="input_raw/"
+opraw_path="output_raw/"
 op_path="output/"
 
 for filename in os.listdir(ip_path):
-    filename=os.path.splitext(filename)[0]
-    #print(filename)
-    subprocess.call(["sox","-r","44100","-e","signed","-b","16","-c","1",ip_path+filename+".raw",op_path+filename+".wav"])
+    f1=os.path.splitext(filename)[0]
+    f2=os.path.splitext(filename)[1]
+    subprocess.call(["ffmpeg","-i",ip_path+filename,"-f","s16le","-acodec","pcm_s16le",ipraw_path+f1+".raw"])
+    subprocess.call(["./examples/rnnoise_demo", ipraw_path+f1+".raw", opraw_path+f1+".raw"])
+    subprocess.call(["sox","-r","44100","-e","signed","-b","16","-c","1",opraw_path+f1+".raw",op_path+f1+".wav"])
